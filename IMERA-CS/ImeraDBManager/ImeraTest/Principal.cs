6c319f38653a364088b31a0d5c80cc1a357bbe52
MrLinoP <julianlinop@gmail.com>
2024-09-28 23:39:19 -0500

    DBManager y Pabellon

---
+++ IMERA-CS/ImeraDBManager/ImeraTest/Principal.cs
@@ -0,0 +1,17 @@
+﻿using ImeraInfraModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImeraTest
+{
+    internal class Principal
+    {
+        static void Main(string[] args)
+        {
+            Pabellon pabellon = new Pabellon();
+        }
+    }
+}
